1d55a5b2f3d0ac4015c2174549143e5d44f86e8a
Alan Tran <alan.trann@gmail.com>
2015-03-09 15:05:32 -0400

    Changed root route to scripts#index, added table

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :scripts
 
   devise_for :users
-  root 'pages#home'
+  root 'scripts#index'
   get 'about' => "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
