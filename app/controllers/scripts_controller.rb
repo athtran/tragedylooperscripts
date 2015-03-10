71994069a8be6189656d58c07439bf9b85f303e2
Alan Tran <alan.trann@gmail.com>
2015-03-10 00:26:55 -0400

    Added admin attribute to users

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -56,6 +56,7 @@ class ScriptsController < ApplicationController
       redirect_to scripts_path, notice: "Not authorized to edit this script" if @script.nil?
     end
 
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def script_params
       params.require(:script).permit(:hints, :victory_conditions, :title, :description, :cast, :incident, :main_plot, :subplot_1, :subplot_2, :tragedy_set, :num_loops, :days_loop, :special_rule, :cast_1, :cast_2, :cast_3, :cast_4, :cast_5, :cast_6, :cast_7, :cast_8, :cast_9, :cast_10, :cast_11, :cast_12, :role_1, :role_2, :role_3, :role_4, :role_5, :role_6, :role_7, :role_8, :role_9, :role_10, :role_11, :role_12, :day_1, :day_2, :day_3, :day_4, :day_5, :day_6, :day_7, :day_8, :culprit_1, :culprit_2, :culprit_3, :culprit_4, :culprit_5, :culprit_6, :culprit_7, :culprit_8)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+ 
   resources :scripts do
     get 'mastermind', on: :member
   end
