a4bfc1acb9723dccb4f9bbe2d0b1d9ecb84e6671
kproudler <kevinproudler@gmail.com>
2020-10-27 15:30:38 -0400

    Rails 5 upgrade

--- app/models/script.rb
+++ app/models/script.rb
@@ -1,4 +1,4 @@
-class Script < ActiveRecord::Base
+class Script < ApplicationRecord
 	belongs_to :user
 	has_many :comments, :dependent=> :destroy
 end
--- app/models/tset.rb
+++ app/models/tset.rb
@@ -1,4 +1,4 @@
-class Tset < ActiveRecord::Base
+class Tset < ApplicationRecord
 	belongs_to :user
 	has_attached_file :attachment_1
 	validates_attachment_content_type :attachment_1, :content_type => ["application/pdf"] 
