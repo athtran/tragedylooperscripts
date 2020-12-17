677d63f8edaa3096f80cc518338601beda4b4ddd
kproudler <kevinproudler@gmail.com>
2020-12-16 21:19:48 -0500

    pagination + removed old columns

--- app/models/script.rb
+++ app/models/script.rb
@@ -3,4 +3,5 @@ class Script < ApplicationRecord
 	has_many :comments, :dependent=> :destroy
 	has_many :casts, :dependent=> :destroy
 	accepts_nested_attributes_for :casts
+	default_scope { order(created_at: :desc) }
 end
